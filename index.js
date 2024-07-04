2de36c537d143538947c5a799f64dc95bdb64195
Shyam Agarwal <shyamagarwal3800@gmail.com>
2024-07-05 00:20:08 +0530

    data added

--- index.js
+++ index.js
@@ -3,11 +3,19 @@ const express = require('express');
 const app = express()
 const port = 4000
 
+const data = {
+    "name" :"Shyam",
+    "age" : 19
+}
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 app.get('/shyam',(req,res) => {
     res.send("hi my self Shyam Agarwal")
+})
+app.get('/info',(req,res)=>{
+    res.json(data);
 })
 
 app.listen(process.env.PORT, () => {
